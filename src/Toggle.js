931997cf59a8d9116620909efb118e5a353182a3
Nick Trabue <ntrabue@users.noreply.github.com>
2019-10-20 14:04:13 -0500

    Fixed Buttons

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,26 +2,39 @@ import React from "react";
 import { ButtonGroup, Button } from "@material-ui/core";
 import { useAppReducer, useAppState } from "./State";
 import { Explore, Person } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  buttonGroup: {
+    margin: "20px"
+  },
+  buttonIcon: {
+    marginRight: "15px"
+  }
+}));
 
 const ToggleView = () => {
+  const classes = useStyles();
   const { view } = useAppState();
   const dispatch = useAppReducer();
 
   return (
-    <ButtonGroup>
+    <ButtonGroup className={classes.buttonGroup}>
       <Button
         color={view === "community" && "primary"}
         variant="contained"
         onClick={() => dispatch({ type: "setView", view: "community" })}
       >
-        <Explore />
+        <Explore className={classes.buttonIcon} />
+        Community
       </Button>
       <Button
         color={view === "personal" && "primary"}
         variant="contained"
         onClick={() => dispatch({ type: "setView", view: "personal" })}
       >
-        <Person />
+        <Person className={classes.buttonIcon} />
+        My Pins
       </Button>
     </ButtonGroup>
   );
